ffdfd05a8a4d7b91e50aae0f7ea00b281801bbf2
Fabio-Ricci <fabioricci9@gmail.com>
2018-11-14 15:50:47 -0200

    funcionando

--- src/loco/ronda.c
+++ src/loco/ronda.c
@@ -1,45 +1,62 @@
 #include "api_robot.h"
 
 int _start() {
-	set_motor_speed(0, 60);
-	set_motor_speed(1, 60);
+	set_motor_speed(0, 10);
+	set_motor_speed(1, 10);
 	int actualTime = get_time();
 	int timeBase = actualTime;
+	int timeBaseAux;
+	int aux;
 	int i = 1;
-	int sonar = 0;
+	int sonar3 = 2000;
+	int sonar4 = 2000;
+	int wall = 0;
 
 	while (1) {
 		if (i==50){
 			i=1;
-		} 
+		}
+		set_time(0);
 
 		/*vira*/
-		actualTime = get_time();
-		set_motor_speed(0, 0);
-		while (actualTime < timeBase + 180){
+		if (!wall) {
 			actualTime = get_time();
+			timeBase = actualTime;
+			set_motor_speed(0, 0);
+			while (actualTime < timeBase + 1950) {
+				actualTime = get_time();
+			}
+			set_motor_speed(0, 10);
 		}
-		timeBase = actualTime;	
-		set_motor_speed(0, 60);
-
-		//sonar = read_sonar(4);
-		//if (i*1000 > sonar && sonar > 1200) {
 		/*reto*/
 		actualTime = get_time();
-		while (actualTime < timeBase + i*100){
+		timeBase = actualTime;	
+		aux = actualTime;
+		while (actualTime < timeBase + i) {
+			if (actualTime != aux) {
+				aux = actualTime;
+				sonar3 = read_sonar(3);
+				sonar4 = read_sonar(4);
+			}
+			if (sonar3 < 1500 || sonar4 < 1500) {
+				/*sonar*/
+				wall = 1;
+				actualTime = get_time();
+				timeBaseAux = actualTime;
+				set_motor_speed(0, 0);
+				while (actualTime < timeBaseAux + 1950){
+					actualTime = get_time();
+				}
+				set_motor_speed(0, 10);
+				sonar3 = read_sonar(3);
+				sonar4 = read_sonar(4);
+				break;
+			} else {
+				wall = 0;
+			}
 			actualTime = get_time();
 		}
-		timeBase = actualTime;
 		i++;
-		// } else {
-		// 	/*sonar*/
-		// 	actualTime = get_time();
-		// 	set_motor_speed(0, 0);
-		// 	while (actualTime < timeBase + 370){
-		// 		actualTime = get_time();
-		// 	}
-		// 	set_motor_speed(0, 60);
-		// }
 	}
 
 	return 0;
